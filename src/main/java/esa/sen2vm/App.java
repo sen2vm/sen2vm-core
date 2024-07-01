f93f9c474abf539950f03430a2df7e225ee2c423
Sara TERAITETIA <sara.teraitetia@csgroup.eu>
2024-07-01 10:51:48 +0200

    enh: first implementation of main class

--- src/main/java/esa/sen2vm/App.java
+++ src/main/java/esa/sen2vm/App.java
@@ -1,7 +1,7 @@
 package esa.sen2vm;
 
 /**
- * Hello world!
+ * Main class
  *
  */
 public class App
@@ -9,5 +9,36 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        if (args.length < 2) {
+            System.out.println("Usage: java Main -c <mandatoryArg> [-p optionalArg]");
+            return;
+        }
+
+        String mandatoryArg = null;
+        String optionalArg = null;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-c".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    mandatoryArg = args[i + 1];
+                }
+            } else if (i > 0 && args[i - 1].equals("-p")) {
+                optionalArg = args[i];
+            }
+        }
+
+        if (mandatoryArg == null) {
+            System.out.println("Usage: java Main -c <mandatoryArg> [-p optionalArg]");
+            return;
+        }
+
+        System.out.println("Mandatory Argument: " + mandatoryArg);
+        if (optionalArg != null) {
+            System.out.println("Optional Argument: " + optionalArg);
+        } else {
+            System.out.println("No Optional Argument provided");
+        }
+
     }
 }
