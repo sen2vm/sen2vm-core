1cac454ecaaabdee60555310691c9678a549fb4e
Sara TERAITETIA <sara.teraitetia@csgroup.eu>
2024-09-26 11:03:14 +0200

    remove useless import

--- src/main/java/esa/sen2vm/utils/Utils.java
+++ src/main/java/esa/sen2vm/utils/Utils.java
@@ -1,38 +1,20 @@
 package esa.sen2vm.utils;
 
 import org.orekit.data.DataContext;
-import org.orekit.data.LazyLoadedDataContext;
-import org.orekit.forces.gravity.potential.GravityFieldFactory;
 import org.orekit.frames.EOPEntry;
 import org.orekit.frames.EOPHistoryLoader;
 import org.orekit.frames.FramesFactory;
 import org.orekit.frames.ITRFVersion;
-import org.orekit.models.earth.weather.GlobalPressureTemperature2Model;
-import org.orekit.orbits.FieldCartesianOrbit;
-import org.orekit.orbits.FieldCircularOrbit;
-import org.orekit.orbits.FieldEquinoctialOrbit;
-import org.orekit.orbits.FieldKeplerianOrbit;
-import org.orekit.propagation.semianalytical.dsst.utilities.JacobiPolynomials;
-import org.orekit.propagation.semianalytical.dsst.utilities.NewcombOperators;
 import org.orekit.time.AbsoluteDate;
-import org.orekit.time.DateComponents;
 import org.orekit.time.DateTimeComponents;
-import org.orekit.time.GNSSDate;
 import org.orekit.time.TimeComponents;
 import org.orekit.time.TimeScale;
-import org.orekit.time.TimeScalesFactory;
 import org.orekit.utils.Constants;
 import org.orekit.utils.IERSConventions;
 
-import java.lang.Math;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
-import java.util.concurrent.atomic.AtomicReference;
 
 import esa.sen2vm.utils.Sen2VMConstants;
 
