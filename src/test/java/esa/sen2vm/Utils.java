b403d1d8315f7e71b54f9fff162918b9ed7d0053
marine.bouchet <marine.bouchet@csgroup.eu>
2025-03-07 15:09:36 +0100

    fix: error calculation for inverse loc

--- src/test/java/esa/sen2vm/Utils.java
+++ src/test/java/esa/sen2vm/Utils.java
@@ -84,7 +84,7 @@ public class Utils {
     }
 
     private static final double THRESHOLD_DIR = 1e-8;
-    private static final double THRESHOLD_INV = 1e-3; // todo
+    private static final double THRESHOLD_INV = 1e-6; // todo
 
     public static void verifyStepDirectLoc(String configFilepath, int step) throws Sen2VMException
     {
@@ -157,7 +157,7 @@ public class Utils {
                 if (grid != null) {
                     int len = grid.toPath().getNameCount();
                     String refGrid = outputRef + File.separator + grid.toPath().subpath(len - 4, len);
-                    assertEquals(imagesEqual(grid.toString(), refGrid,THRESHOLD_DIR), true);
+                    assertEquals(imagesEqualDirect(grid.toString(), refGrid,THRESHOLD_DIR), true);
                 }
                 b = b + 1;
             }
@@ -176,10 +176,12 @@ public class Utils {
         {
             for(int b = 0; b < Sen2VMConstants.NB_BANDS; b++)
             {
+                BandInfo bandInfo = BandInfo.getBandInfoFromIndex(b);
+                float res = (float) bandInfo.getPixelHeight();
                 if (outputGrids[d][b] != null) {
                     File outputGrid = outputGrids[d][b] ;
                     File refGrid = refGrids[d][b] ;
-                    assertEquals(imagesEqual(outputGrid.toString(), refGrid.toString(), THRESHOLD_INV), true);
+                    assertEquals(imagesEqualInverse(outputGrid.toString(), refGrid.toString(), THRESHOLD_INV, res), true);
                 }
             }
         }
@@ -187,45 +189,98 @@ public class Utils {
 
 
 
-    public static boolean imagesEqual(String img1Path, String img2Path, double threshold) throws IOException{
+    public static boolean imagesEqualDirect(String img1Path, String img2Path, double threshold) throws IOException{
 
         Dataset ds1 = gdal.Open(img1Path, 0);
         Dataset ds2 = gdal.Open(img2Path, 0);
         if (ds1.GetRasterCount() == ds2.GetRasterCount() && ds1.getRasterXSize() == ds2.getRasterXSize() && ds1.getRasterYSize() == ds2.getRasterYSize()) {
+
             for(int b = 1; b <= ds1.GetRasterCount(); b++)
             {
-
                 Band b1 = ds1.GetRasterBand(b);
                 Band b2 = ds2.GetRasterBand(b);
 
                 for(int r = 0; r < ds1.getRasterYSize(); r++) {
-
                     double[] data1 = new double[ds1.getRasterXSize()];
                     b1.ReadRaster(0, r, ds1.getRasterXSize(), 1, data1);
                     double[] data2 = new double[ds2.getRasterXSize()];
                     b2.ReadRaster(0, r, ds2.getRasterXSize(), 1, data2);
+
                     for(int c = 0; c < ds1.getRasterXSize(); c++) {
                         if (!(Double.isNaN(data1[c]) == Double.isNaN(data2[c])))
                         {
                             return false;
                         }
-                        if (!(Double.isNaN(data1[c]))  && Math.abs(data1[c] - data2[c]) < threshold) {
-                            System.out.println("[DEBUG] " + img1Path + " " + img2Path);
-                            String error = String.valueOf(data1[c]) + " - " + String.valueOf(data1[c]) + " = " + String.valueOf(data1[c] - data2[c]);
-                            System.out.println("[DEBUG] (" + String.valueOf(r) + "," + String.valueOf(c) + "): " + error);
 
-                        }
-
-                        if (!(Double.isNaN(data1[c]))  && Math.abs(data1[c] - data2[c]) > threshold)
-                        {
-                            System.out.println("[DEBUG] " + img1Path + " " + img2Path);
+                        if (!(Double.isNaN(data1[c]))  && Math.abs(data1[c] - data2[c]) > threshold) {
+                            System.out.println("Error in " + img1Path);
                             String error = String.valueOf(data1[c]) + " - " + String.valueOf(data1[c]) + " = " + String.valueOf(data1[c] - data2[c]);
-                            System.out.println("[DEBUG] (" + String.valueOf(r) + "," + String.valueOf(c) + "): " + error);
+                            System.out.println("Band "+ String.valueOf(b) + "/ coordinates (" + String.valueOf(r) + "," + String.valueOf(c) + "): " + error);
                             return false;
                         }
                     }
                 }
             }
+            return true;
+        }
+        return false;
+    }
+
+     public static boolean imagesEqualInverse(String img1Path, String img2Path, double threshold, Float res) throws IOException{
+
+        Dataset ds1 = gdal.Open(img1Path, 0);
+        Dataset ds2 = gdal.Open(img2Path, 0);
+        if (ds1.GetRasterCount() == ds2.GetRasterCount() && ds1.getRasterXSize() == ds2.getRasterXSize() && ds1.getRasterYSize() == ds2.getRasterYSize()) {
+
+            Band ds1b1 = ds1.GetRasterBand(1);
+            Band ds1b2 = ds1.GetRasterBand(2);
+            Band ds2b1 = ds2.GetRasterBand(1);
+            Band ds2b2 = ds2.GetRasterBand(2);
+
+            for(int r = 0; r < ds1.getRasterYSize(); r++) {
+
+                double[] data1b1 = new double[ds1.getRasterXSize()];
+                ds1b1.ReadRaster(0, r, ds1.getRasterXSize(), 1, data1b1);
+                double[] data1b2 = new double[ds1.getRasterXSize()];
+                ds1b2.ReadRaster(0, r, ds1.getRasterXSize(), 1, data1b2);
+
+                double[] data2b1 = new double[ds1.getRasterXSize()];
+                ds2b1.ReadRaster(0, r, ds1.getRasterXSize(), 1, data2b1);
+                double[] data2b2 = new double[ds1.getRasterXSize()];
+                ds2b2.ReadRaster(0, r, ds1.getRasterXSize(), 1, data2b2);
+
+                for(int c = 0; c < ds1.getRasterXSize(); c++) {
+
+                    // nan in one grid and value in other grid case
+                    if (!(Double.isNaN(data1b1[c]) == Double.isNaN(data2b1[c])))
+                    {
+                        return false;
+                    }
+
+                    // values in both grids
+                    if (!(Double.isNaN(data1b1[c]))) {
+
+                        // Calculation planar error
+                        double diff_column = data1b1[c] - data2b1[c] ;
+                        double diff_column_2 = diff_column * diff_column;
+                        double diff_line = data1b2[c] - data2b2[c] ;
+                        double diff_line_2 = diff_line * diff_line;
+                        double diff = Math.sqrt(diff_line_2 + diff_column_2);
+                        diff = diff * res ;
+
+                        if (diff > threshold) {
+                            System.out.println("Error in " + img1Path);
+                            String error = "(" + String.valueOf(data1b2[c]) + ", " + String.valueOf(data1b1[c])  + ")";
+                            error = error + " vs (" + String.valueOf(data2b2[c]) + ", " + String.valueOf(data2b1[c]) + ")";
+                            error = error + " = " + String.valueOf(diff);
+                            System.out.println("Coordinates (" + String.valueOf(r) + "," + String.valueOf(c) + "): " + error);
+                            return false;
+                        }
+                    }
+
+                }
+            }
+
             return true;
         }
         return false;
